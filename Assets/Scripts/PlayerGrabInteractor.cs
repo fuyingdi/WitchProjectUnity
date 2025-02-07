16cd812ad478422e276e0f658f70ffac7f8bf5f9
fuyingdi <fuyingdi@126.com>
2025-02-07 12:05:58 +0800

    update

---
+++ Assets/Scripts/PlayerGrabInteractor.cs
@@ -0,0 +1,69 @@
+﻿using HutongGames.PlayMaker;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Assets.Scripts
+{
+    public class PlayerGrabInteractor : MonoBehaviour
+    {
+        public PlayerGrabSensor Sensor;
+        public Transform Hand;
+        [Readonly]
+        public GameObject CurrentInHand;
+
+        private void Start()
+        {
+            GameManager.Ins.Input.InputActions.PlayerControls.Interact.started += Interact;
+
+            // TODO:
+            //GameManager.Ins.Input.InputActions.PlayerControls.Throw.started += ThrowAim;
+            //GameManager.Ins.Input.InputActions.PlayerControls.Throw.canceld += Throw;
+        }
+
+        private void Interact(InputAction.CallbackContext ctx)
+        {
+            // drop
+            if (CurrentInHand != null)
+            {
+                CurrentInHand = null;
+                return;
+            }
+
+            // catch
+            if (Sensor.CurrentHold.Count > 0)
+            {
+                var target = Sensor.CurrentHold[0];
+                CurrentInHand = target;
+                target.transform.position = Hand.transform.position;
+            }
+
+            // TODO: put on table
+            if (CurrentInHand != null)
+            {
+
+            }
+        }
+
+        private void ThrowAim(InputAction.CallbackContext ctx)
+        {
+            // Lock Move
+
+            // Show Projectile Trail
+        }
+
+        private void Throw(InputAction.CallbackContext ctx)
+        {
+            // UnLock Move
+
+            // AddForceTo Target
+        }
+
+        private void Update()
+        {
+            if (CurrentInHand != null)
+            {
+                CurrentInHand.transform.position = Hand.transform.position;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerGrabSensor.cs
@@ -0,0 +1,43 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGrabSensor : MonoBehaviour
+{
+    public string TagFilter;
+
+    [ReadOnly] public List<GameObject> CurrentHold;
+
+    private void Start()
+    {
+        CurrentHold = new List<GameObject>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject);
+        if (!other.CompareTag(TagFilter.ToString()))
+        {
+            return;
+        }
+        else
+        {
+            CurrentHold.Add(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (TagFilter != null)
+        {
+            if (!other.CompareTag(TagFilter.ToString()))
+            {
+                return;
+            }
+            else
+            {
+                CurrentHold.Remove(other.gameObject);
+            }
+        }
+    }
+}
